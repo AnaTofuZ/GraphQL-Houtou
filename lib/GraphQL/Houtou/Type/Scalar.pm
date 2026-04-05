cc303554d5f836bc9b7691183b47f93ab8cc46ef
anatofuz <anatofuz@gmail.com>
2026-04-06 08:29:06 +0900

    Add fast paths for built-in scalar coercion

--- lib/GraphQL/Houtou/Type/Scalar.pm
+++ lib/GraphQL/Houtou/Type/Scalar.pm
@@ -6,9 +6,9 @@ use warnings;
 
 use Moo;
 use Exporter 'import';
-use GraphQL::Houtou::Type::Library -all;
 use JSON::MaybeXS qw(JSON is_bool);
-use Types::Standard -all;
+use Scalar::Util qw(looks_like_number);
+use Types::Standard qw(CodeRef);
 
 extends 'GraphQL::Houtou::Type';
 with qw(
@@ -39,65 +39,177 @@ sub _leave_undef {
 
 has serialize => (is => 'ro', isa => CodeRef, required => 1);
 has parse_value => (is => 'ro', isa => CodeRef);
+has _builtin_kind => (is => 'ro');
+
+sub _fast_is_nonref_defined {
+  defined $_[0] && !ref($_[0]);
+}
+
+sub _fast_is_int32_signed {
+  return if !_fast_is_nonref_defined($_[0]);
+  return if !looks_like_number($_[0]);
+  return if int($_[0]) != $_[0];
+  return $_[0] >= -2147483648 && $_[0] <= 2147483647;
+}
+
+sub _fast_is_num {
+  return if !_fast_is_nonref_defined($_[0]);
+  return looks_like_number($_[0]);
+}
+
+sub _fast_is_str {
+  return _fast_is_nonref_defined($_[0]);
+}
+
+sub _fast_is_boolish {
+  return 1 if is_bool($_[0]);
+  return if !_fast_is_nonref_defined($_[0]);
+  return if !looks_like_number($_[0]);
+  return $_[0] == 0 || $_[0] == 1;
+}
+
+sub _builtin_is_valid {
+  my ($self, $item) = @_;
+  my $kind = $self->_builtin_kind or return;
+
+  return 1 if !defined $item;
+  return _fast_is_int32_signed($item) if $kind eq 'Int';
+  return _fast_is_num($item) if $kind eq 'Float';
+  return _fast_is_str($item) if $kind eq 'String';
+  return _fast_is_boolish($item) if $kind eq 'Boolean';
+  return _fast_is_str($item) if $kind eq 'ID';
+  return;
+}
+
+sub _builtin_graphql_to_perl {
+  my ($self, $item) = @_;
+  my $kind = $self->_builtin_kind or return;
+
+  return $item if !defined $item;
+
+  if ($kind eq 'Int') {
+    _fast_is_int32_signed($item) or die "Not an Int.\n";
+    return $item + 0;
+  }
+  if ($kind eq 'Float') {
+    _fast_is_num($item) or die "Not a Float.\n";
+    return $item + 0;
+  }
+  if ($kind eq 'String') {
+    _fast_is_str($item) or die "Not a String.\n";
+    return $item;
+  }
+  if ($kind eq 'Boolean') {
+    _fast_is_boolish($item) or die "Not a Boolean.\n";
+    return $item ? 1 : 0;
+  }
+  if ($kind eq 'ID') {
+    _fast_is_str($item) or die "Not an ID.\n";
+    return $item;
+  }
+
+  return;
+}
+
+sub _builtin_perl_to_graphql {
+  my ($self, $item) = @_;
+  my $kind = $self->_builtin_kind or return;
+
+  return $item if !defined $item;
+
+  if ($kind eq 'Int') {
+    _fast_is_int32_signed($item) or die "Not an Int.\n";
+    return $item + 0;
+  }
+  if ($kind eq 'Float') {
+    _fast_is_num($item) or die "Not a Float.\n";
+    return $item + 0;
+  }
+  if ($kind eq 'String') {
+    _fast_is_str($item) or die "Not a String.\n";
+    return $item . '';
+  }
+  if ($kind eq 'Boolean') {
+    _fast_is_boolish($item) or die "Not a Boolean.\n";
+    return $item ? JSON->true : JSON->false;
+  }
+  if ($kind eq 'ID') {
+    _fast_is_str($item) or die "Not an ID.\n";
+    return $item . '';
+  }
+
+  return;
+}
 
 sub is_valid {
   my ($self, $item) = @_;
+  my $fast = $self->_builtin_kind ? $self->_builtin_is_valid($item) : undef;
+  return $fast if defined $fast;
   return 1 if !defined $item;
   return eval { $self->serialize->($item); 1 };
 }
 
 sub graphql_to_perl {
   my ($self, $item) = @_;
+  my $fast = $self->_builtin_kind ? $self->_builtin_graphql_to_perl($item) : undef;
+  return $fast if defined $fast || !defined $item;
   return $self->parse_value->($item);
 }
 
 sub perl_to_graphql {
   my ($self, $item) = @_;
+  my $fast = $self->_builtin_kind ? $self->_builtin_perl_to_graphql($item) : undef;
+  return $fast if defined $fast || !defined $item;
   return $self->serialize->($item);
 }
 
 our $Int = __PACKAGE__->new(
+  _builtin_kind => 'Int',
   name => 'Int',
   description =>
     'The `Int` scalar type represents non-fractional signed whole numeric ' .
     'values. Int can represent values between -(2^31) and 2^31 - 1.',
-  serialize => _leave_undef(sub { !is_Int32Signed($_[0]) and die "Not an Int.\n"; $_[0] + 0 }),
-  parse_value => _leave_undef(sub { !is_Int32Signed($_[0]) and die "Not an Int.\n"; $_[0] + 0 }),
+  serialize => _leave_undef(sub { !_fast_is_int32_signed($_[0]) and die "Not an Int.\n"; $_[0] + 0 }),
+  parse_value => _leave_undef(sub { !_fast_is_int32_signed($_[0]) and die "Not an Int.\n"; $_[0] + 0 }),
 );
 
 our $Float = __PACKAGE__->new(
+  _builtin_kind => 'Float',
   name => 'Float',
   description =>
     'The `Float` scalar type represents signed double-precision fractional ' .
     'values as specified by [IEEE 754](http://en.wikipedia.org/wiki/IEEE_floating_point).',
-  serialize => _leave_undef(sub { !is_Num($_[0]) and die "Not a Float.\n"; $_[0] + 0 }),
-  parse_value => _leave_undef(sub { !is_Num($_[0]) and die "Not a Float.\n"; $_[0] + 0 }),
+  serialize => _leave_undef(sub { !_fast_is_num($_[0]) and die "Not a Float.\n"; $_[0] + 0 }),
+  parse_value => _leave_undef(sub { !_fast_is_num($_[0]) and die "Not a Float.\n"; $_[0] + 0 }),
 );
 
 our $String = __PACKAGE__->new(
+  _builtin_kind => 'String',
   name => 'String',
   description =>
     'The `String` scalar type represents textual data, represented as UTF-8 ' .
     'character sequences. The String type is most often used by GraphQL to ' .
     'represent free-form human-readable text.',
-  serialize => _leave_undef(sub { !is_Str($_[0]) and die "Not a String.\n"; $_[0] . '' }),
-  parse_value => _leave_undef(sub { !is_Str($_[0]) and die "Not a String.\n"; $_[0] }),
+  serialize => _leave_undef(sub { !_fast_is_str($_[0]) and die "Not a String.\n"; $_[0] . '' }),
+  parse_value => _leave_undef(sub { !_fast_is_str($_[0]) and die "Not a String.\n"; $_[0] }),
 );
 
 our $Boolean = __PACKAGE__->new(
+  _builtin_kind => 'Boolean',
   name => 'Boolean',
   description => 'The `Boolean` scalar type represents `true` or `false`.',
   serialize => _leave_undef(sub {
-    !is_Bool($_[0]) && !is_bool($_[0]) and die "Not a Boolean.\n";
+    !_fast_is_boolish($_[0]) and die "Not a Boolean.\n";
     $_[0] ? JSON->true : JSON->false;
   }),
   parse_value => _leave_undef(sub {
-    !is_Bool($_[0]) && !is_bool($_[0]) and die "Not a Boolean.\n";
+    !_fast_is_boolish($_[0]) and die "Not a Boolean.\n";
     $_[0] + 0;
   }),
 );
 
 our $ID = __PACKAGE__->new(
+  _builtin_kind => 'ID',
   name => 'ID',
   description =>
     'The `ID` scalar type represents a unique identifier, often used to ' .
@@ -105,8 +217,8 @@ our $ID = __PACKAGE__->new(
     'response as a String; however, it is not intended to be human-readable. ' .
     'When expected as an input type, any string (such as `"4"`) or integer ' .
     '(such as `4`) input value will be accepted as an ID.',
-  serialize => _leave_undef(sub { Str->(@_); $_[0] . '' }),
-  parse_value => _leave_undef(sub { Str->(@_); $_[0] }),
+  serialize => _leave_undef(sub { !_fast_is_str($_[0]) and die "Not an ID.\n"; $_[0] . '' }),
+  parse_value => _leave_undef(sub { !_fast_is_str($_[0]) and die "Not an ID.\n"; $_[0] }),
 );
 
 1;
